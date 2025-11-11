e607cd149ae5934ab168272ffc28439e7c1ba914
Nicolas Dominguez <nicolas.dominguez.it@outlook.com>
2025-11-11 13:02:37 -0300

    Se agrega validaciones al modelo Persona

--- Acudir.Challenge/Acudir.Domain/Models/Persona.cs
+++ Acudir.Challenge/Acudir.Domain/Models/Persona.cs
@@ -5,13 +5,19 @@ namespace Acudir.Api.Domain.Models
     public class Persona
     {
         public int Id { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "El nombre es obligatorio.")]
         public string Nombre { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "El apellido es obligatorio.")]
         public string Apellido { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "El email es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Debe ser un email válido.")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "La edad es obligatoria.")]
+        [Range(18, int.MaxValue, ErrorMessage = "Debe ser mayor o igual a 18 años.")]
         public int Edad { get; set; }
     }
 }
